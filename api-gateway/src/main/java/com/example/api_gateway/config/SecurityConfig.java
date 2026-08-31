bbb603c2a90dfa49d01e0b2ceefab12d2ed074ea
KirillTepechin <90400949+KirillTepechin@users.noreply.github.com>
2026-09-01 02:07:28 +0400

    helm + frontend-app (#9)
    * helm
    
    * frontend-app
    
    * frontend-app
    
    ---------
    
    Co-authored-by: kirill.tepechin <kir.tep18@mail.com>

--- api-gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
@@ -59,7 +59,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://localhost:8080",
+                "https://ecommerce.local",
+                "http://localhost"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
